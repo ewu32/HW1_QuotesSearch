36b7c73ee854043f156ffcbdfa51f1c8144b54de
unknown <a02349662@usu.edu>
2023-01-29 19:03:55 -0700

    Added base path to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
   build: {
     outDir: "docs" // tells vite to output the build to a folder called docs
   },
+  base: './' // tells vite to request dependencies relative to the path the app is hosted on, not the root domain
 })
